659994d6ebfb89366ccbd0e4bfdc4f2370b043fc
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2023-05-30 22:24:08 +0500

    admin and models v1

--- MarketSport/apps/store/models.py
+++ MarketSport/apps/store/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.conf import settings
+import os
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=60, unique=True, db_index=True)
+    slug = models.SlugField(unique=True, db_index=True, blank=True)
+    
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120, verbose_name='Название')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    slug = models.SlugField()
+    descriptions = models.TextField(blank=True)
+    price = models.FloatField(verbose_name='Цена')
+    image = models.ImageField(blank=True)
+    avalible = models.BooleanField(default=True, verbose_name='В наличии')
+    date_add = models.DateField(auto_now_add=True, verbose_name='Дата добавления')
+    date_update = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары' 
+    
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        file_name = self.category.title
+        
+        self.image.name = file_name + "/" + self.title + ".png"
+        
+        super(Product, self).save(*args, **kwargs)
+
+        
