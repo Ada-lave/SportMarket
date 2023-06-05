6d576c5716056f9837fb350fd224af050903d7d6
JsPiglet <50877311+Arsenka228@users.noreply.github.com>
2023-06-05 23:55:51 +0500

    DDOS2

--- MarketSport/static/js/form/valid.js
+++ MarketSport/static/js/form/valid.js
@@ -1,4 +1,43 @@
+function createError(input) {
+  const error = document.createElement("div");
+  error.classList.add("error");
+  input.parentNode.insertBefore(error, input.nextSibling);
 
-document.querySelector('.FormList').addEventListener('sumbit',function(event){
-    event.preventDefault;
-})
+  input.classList.add("error");
+  input.style.border = '1px solid red';
+}
+
+function removeError(input) {
+  input.classList.remove("error");
+  const error = input.nextElementSibling;
+  if (error && error.classList.contains("error")) {
+    input.parentNode.removeChild(error);
+    input.style.border = '';
+  }
+}
+
+function validation(form) {
+  const AllInput = form.querySelectorAll('input');
+  let isFormValid = true;
+
+  for (const input of AllInput) {
+    if (input.value.trim() === "") {
+      createError(input);
+      isFormValid = false;
+    } else {
+      removeError(input);
+    }
+  }
+
+  return isFormValid;
+}
+
+const form = document.querySelector("#add-form");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  if (validation(form)) {
+    
+  }
+});
